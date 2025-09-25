5e48ae865b36bf14ba586fac250b900ae727fd00
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-09-25 13:38:03 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "allowed_example" {
-  ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョンの例)
-  instance_type = "t2.micro"
+  ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
+  instance_type = "t3.micro"
   tags = {
     Name = "allowed-instance"
   }
