df01e4f7f8f5696a09326ec2268f3518388fe533
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-09-29 15:14:19 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "allowed_example" {
+resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
   instance_type = "t3.micro"
   tags = {
-    Name = "allowed-instance"
+    Name = "handson-{ご自身のお名前}-instance"
   }
 }
