b46406a9a4d18b5cd73822f0b0e5901a661b8d50
ywakabayashi1414 <yuuki.wakabayashi.tr@kamposol.jp-life.jp>
2025-09-30 13:49:55 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
   instance_type = "t3.micro"
   tags = {
-    Name = "handson-{ywakabayashi}-instance"
+    Name = "handson-ywakabayashi-instance"
   }
 }
