0049d5f8f03aba8340f7d9561e797fab3a71d55a
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-09-30 13:48:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
   instance_type = "t3.micro"
   tags = {
-    Name = "handson-{ご自身のお名前}-instance"
+    Name = "handson-{ご自身のお名前}-instance-day2"
   }
 }
